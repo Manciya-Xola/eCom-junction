8e5d0e55990862d19eb8d527fc6d3bdb8f57f9e3
Xola Manciya <xola.manciya@yoco.com>
2022-04-08 12:41:49 +0200

    changed the size of the product card to accommodate the screen size

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+
+async function getProducts() {
+  const products = await fetch("https://yoco-students-api-server.herokuapp.com/v1/junction/");
+  const json_Products = await products.json()
+  console.log(json_Products);
+}
+getProducts();
