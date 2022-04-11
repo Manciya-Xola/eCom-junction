cb204dc651e418114b96c342754a7b4701d71b90
Xola Manciya <xola.manciya@yoco.com>
2022-04-11 13:24:54 +0200

    imported the dofetch and used it in the main class

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+import { API_URL } from "./shared.js";
+import { doFetch } from "./utils.js";
+
+
+
 async function main(url) {
-  
+  const product = await doFetch(API_URL);
+  console.log(product);
 }
 main();
